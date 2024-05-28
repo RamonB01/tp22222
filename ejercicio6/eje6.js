d5d381d3dc03cdb53d0b7c316da2e8b967547741
Ramon Barrera <rebcontacto@gmail.com>
2024-05-28 19:44:35 -0300

    Ejercicio 6 completo

--- ejercicio6/eje6.js
+++ ejercicio6/eje6.js
@@ -1,4 +1,35 @@
 const formulario = $("formulario");
+const mensaje = $("mensaje");
+const edad = $("edad");
+const correo = $("correo");
+
+// formulario.addEventListener("submit", (e) => {
+//     //Enviar formulario
+//     e.preventDefault();
+// });
+
+formulario.addEventListener("focusout", () => {
+    mensaje.style.color = "red";
+    if (formulario["nombre"].value === "") {
+        mensaje.textContent = "Nombre vacío";
+    } else if (formulario["apellido"].value === "") {
+        mensaje.textContent = "Apellido vacío";
+    } else if (formulario["edad"].value < 0) {
+        mensaje.textContent = "La edad es negativa";
+    } else if (formulario["edad"].value === "") {
+        mensaje.textContent = "La edad está vacía";
+    } else if (formulario["edad"].value < 18) {
+        mensaje.textContent = "No valido a menores de edad";
+    } else if (formulario["correo"].value === "") {
+        mensaje.textContent = "Correo electrónico vacío";
+    } else if (!formulario["correo"].value.includes("@")) {
+        mensaje.textContent = "el correo debe tener '@'";
+    } else {
+        mensaje.textContent = "Datos correctos";
+        mensaje.style.color = "green";
+    }
+});
+
 
 
 
