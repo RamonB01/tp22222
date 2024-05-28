b682ac0f4ded0b9cc4687eac75b347c023f8aa1e
Ramon Barrera <rebcontacto@gmail.com>
2024-05-27 23:32:24 -0300

    Se agregó el html de ejercicio5 y se empezó el ejercicio 6 con sus respectivos archivos

---
+++ ejercicio5/eje5.js
@@ -0,0 +1,6 @@
+function sacarD(){
+    const ladA = parseFloat(document.getElementById("ladoA").value);
+    const ladB = document.getElementById("ladoB");
+    const ladC = document.getElementById("ladoC");
+    const valorD = 
+}
